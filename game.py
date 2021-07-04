8d83b58b29f28e717b9552b5ff6add290cf91d15
Akash S <akashsureshmyd@gmail.com>
2021-07-05 02:19:49 +0530

    Optimised the code

--- game.py
+++ game.py
@@ -1,21 +1,25 @@
-x = 3
-while (x<50):
-    x = (((x+3)*2)-1)
-    print("Guess my number\nLets play a Guessing game")
-    y = int (input("Enter the number"))
-    i = 1
-    while (y!=x):
-        if i == 4:
-            print("Oops!You have used all your chances.You lost this game")
-            break
-        elif y>x:
-            print("Your guess is greater than actual number!Try again")
-            y = int(input("Enter the number"))
-        elif y<x:
-            print("Your guess is lower than actual number!Try again")
-            y = int(input("Enter the number"))
-            i+=1
-    if y == x:
-         print("You won\nCongratulations!")
-         print("You wanna try again")
+import random
 
+while True:
+    print("\nNUMBER GUESSING GAME\n")
+    j=int(input("Enter the number of rounds u need:"))
+    y = int (input("\nGuess the number:"))
+    s=random.randint(1,100)
+    i=1
+    while (y!=s):
+        if i==j:
+            print("Oops!You have used all your chances.You lost this game")
+            input("Press any key to continue")
+            break
+        elif y>s:
+            print("Your guess is greater than actual number!Try again")
+            y = int(input("Guess the number:"))
+            i+=1
+        elif y<s:
+            print("Your guess is lower than actual number!Try again")
+            y = int(input("Guess the number:"))
+            i+=1
+    if y == s:
+         print("You won\nCongratulations!")
+         print("You wanna try again?")
+         input("Press any key to continue")
